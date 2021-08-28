1cced6c65f0f56c603772c5862bc1ee2b620ba9e
gunturwidodoh <gunturwidodoh@gmail.com>
2021-08-28 23:18:15 +0700

    fix link href

--- application/views/templates/navbar.php
+++ application/views/templates/navbar.php
@@ -7,7 +7,7 @@
                 <a class="nav-link" data-widget="pushmenu" href="#"><i class="fas fa-bars"></i></a>
             </li>
             <li class="nav-item d-none d-sm-inline-block">
-                <a href="<?php echo base_url(); ?>" class="nav-link">Home</a>
+                <a href="<?php echo site_url(); ?>/home" class="nav-link">Home</a>
             </li>
         </ul>
         <!-- Right navbar links -->
--- application/views/templates/sidebar.php
+++ application/views/templates/sidebar.php
@@ -1,7 +1,7 @@
 <!-- Main Sidebar Container -->
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
-    <a href="<?php echo base_url(); ?>" class="brand-link">
+    <a href="<?php echo site_url(); ?>/home" class="brand-link">
         <img src="<?php echo base_url('assets/dist'); ?>/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
         <span class="brand-text font-weight-light">Project Tracker</span>
     </a>
