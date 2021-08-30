4e53d586fc499105332fe6af970a566b59d24a93
gunturwidodoh <gunturwidodoh@gmail.com>
2021-08-31 00:02:21 +0700

    Update Role Menu

--- application/views/templates/sidebar.php
+++ application/views/templates/sidebar.php
@@ -1,3 +1,16 @@
+<!-- Query Menu -->
+<?php
+$role = $this->session->userdata('role');
+$queryMenu =   "SELECT `user_menu`.`id`, `menu`
+                FROM `user_menu` 
+                JOIN `user_access_menu`
+                ON `user_menu`.`id` = `user_access_menu`.`menu_id`
+                WHERE `user_access_menu`.`role_id` = $role
+                ORDER BY `user_access_menu`.`menu_id` ASC                    
+                ";
+$menu = $this->db->query($queryMenu)->result_array();
+?>
+
 <!-- Main Sidebar Container -->
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
@@ -10,50 +23,35 @@
     <div class="sidebar">
         <!-- Sidebar Menu -->
         <nav class="mt-2">
-            <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
-                <!-- Add icons to the links using the .nav-icon class
-               with font-awesome or any other icon font library -->
-                <li class="nav-item has-treeview">
-                    <a href="#" class="nav-link">
-                        <p>
-                            Admin
-                            <i class="right fas fa-angle-left"></i>
-                        </p>
-                    </a>
+            <!-- Add icons to the links using the .nav-icon class
+            with font-awesome or any other icon font library -->
+            <?php foreach ($menu as $m) : ?>
+                <h2 style="color:white;">
+                    <!-- Looping menu dan submenu -->
+                    <?= $m['menu']; ?>
+                </h2>
+                <!-- Query Sub menu -->
+                <?php
+                $menuId = $m['id'];
+                $querySubMenu =     "SELECT *
+                                        FROM `user_sub_menu` 
+                                        WHERE `user_sub_menu`.`menu_id` = $menuId
+                                        AND `is_active` = 1                  
+                                    ";
+                $subMenu = $this->db->query($querySubMenu)->result_array();
+                ?>
+
+                <?php foreach ($subMenu as $sm) : ?>
                     <ul class="nav nav-treeview">
                         <li class="nav-item">
-                            <a href="<?= site_url() ?>/admin" class="nav-link">
-                                <p>Dashboard</p>
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a href="<?= site_url() ?>/admin/register" class="nav-link">
-                                <p>Register</p>
+                            <a href="<?= site_url($sm['url']); ?>" class="nav-link">
+                                <?= $sm['title']; ?>
                             </a>
                         </li>
                     </ul>
-                </li>
-                <li class="nav-item has-treeview">
-                    <a href="#" class="nav-link">
-                        <p>
-                            User
-                            <i class="right fas fa-angle-left"></i>
-                        </p>
-                    </a>
-                    <ul class="nav nav-treeview">
-                        <li class="nav-item">
-                            <a href="<?= site_url() ?>/home" class="nav-link">
-                                <p>Home</p>
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a href="<?= site_url() ?>/tabel" class="nav-link">
-                                <p>Tabel</p>
-                            </a>
-                        </li>
-                    </ul>
-                </li>
-            </ul>
+
+                <?php endforeach; ?>
+            <?php endforeach; ?>
         </nav>
         <!-- /.sidebar-menu -->
     </div>
